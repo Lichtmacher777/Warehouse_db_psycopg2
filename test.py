ff8c8bdea01456c0e49e82cfe0835aa1969dfc8f
Lichtmacher777 <s.kochkorian@gmx.de>
2023-10-17 14:22:42 +0200

    Added solutions

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import unittest
+import datetime
+
 
 # import functions from solutions
 from solution_1 import read_database_version
@@ -6,21 +8,47 @@ from solution_2 import get_warehouse_detail, get_employee_detail
 from solution_3 import update_employee_experience
 from solution_4 import get_specialist_employee_list
 
+# get the database version information
+database_version = read_database_version()
+
 
 class TestCalcSolution(unittest.TestCase):
-    def test_solution_1(self):
-        # Add the print statement from your solution 1 to the empty string below
-        self.assertEqual(read_database_version, "")
 
-    def test_solution_2(self):
-        # Add the print statements from your solution 2 to the empty string below
-        self.assertEqual(get_employee_detail(105), "")
-        self.assertEqual(get_warehouse_detail(2), "")
+    def test_read_database_version(self):
+        expected_output = database_version
+        actual_output = read_database_version()
 
-    def test_solution_3(self):
-        # Add the print statements from your solution 3 to the empty string below
-        self.assertEqual(update_employee_experience(101), "")
+        self.assertEqual(expected_output, actual_output)
 
-    def test_solution_4(self):
-        # Add the print statements from your solution 4 to the empty string below
-        self.assertEqual(get_specialist_employee_list("Driver", 30000), "")
+    def test_get_employee_detail(self):
+        expected_output = (105, 'Linda', 3, datetime.date(
+            2004, 6, 4), 'Logistics Spcialist', 42000, 19)
+        actual_output = get_employee_detail(105)
+
+        self.assertEqual(expected_output, actual_output)
+
+    def test_get_warehouse_detail(self):
+        expected_output = (2, 'Rewe Warehouse', 400)
+        actual_output = get_warehouse_detail(2)
+
+        self.assertEqual(expected_output, actual_output)
+
+    def test_update_employee_experience(self):
+        expected_output = None
+        actual_output = update_employee_experience(101)
+
+        self.assertEqual(expected_output, actual_output)
+
+    def test_get_specialist_employee_list(self):
+        expected_output = None
+        actual_output = get_specialist_employee_list("Driver", 30000)
+
+        self.assertEqual(expected_output, actual_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
+# --------------------------------
+#     Ran 5 tests in 0.062s
+
+# OK
